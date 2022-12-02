5ab1f33686cc61df692d2e48a2ed145a1bbb6bc7
Markas Klapovščiuk <klapovsciuk@gmail.com>
2022-12-02 13:57:31 +0100

    Move to utils, cause will be reused

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
 	"sort"
-	"strconv"
 	"strings"
+
+	"github.com/markkkkas/advent-of-code/utils"
 )
 
 func main() {
@@ -14,21 +14,13 @@ func main() {
 	sort.Ints(calories)
 	totalElves := len(calories)
 	firstPart := calories[totalElves-1]
-	secondPart := sum(calories[totalElves-3:])
+	secondPart := utils.IntsSum(calories[totalElves-3:])
 	fmt.Println(firstPart, secondPart)
-}
-
-func mustReadInput(fileName string) string {
-	f, err := os.ReadFile(fileName)
-	if err != nil {
-		panic(err)
-	}
-	return string(f)
 }
 
 func mustGroupAndSumEveryElveCalories() []int {
 	var (
-		input    = mustReadInput("./input.txt")
+		input    = utils.MustReadInput("./input.txt")
 		scanner  = bufio.NewScanner(strings.NewReader(input))
 		curr     = 0
 		calories []int
@@ -36,7 +28,7 @@ func mustGroupAndSumEveryElveCalories() []int {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line != "" {
-			curr += mustConvertStringToInt(line)
+			curr += utils.MustConvertStringToInt(line)
 		} else {
 			calories = append(calories, curr)
 			curr = 0
@@ -46,20 +38,4 @@ func mustGroupAndSumEveryElveCalories() []int {
 		panic(err)
 	}
 	return calories
-}
-
-func sum(nums []int) int {
-	s := 0
-	for _, n := range nums {
-		s += n
-	}
-	return s
-}
-
-func mustConvertStringToInt(s string) int {
-	val, err := strconv.Atoi(s)
-	if err != nil {
-		panic(err)
-	}
-	return val
 }
