54c4e2e968eac077d7261fe823cfeffc6dce4115
Markas Klapovščiuk <klapovsciuk@gmail.com>
2023-12-10 12:20:44 +0200

    chore: Boilerplate for day2.rs

--- 2023/src/bin/day2.rs
+++ 2023/src/bin/day2.rs
@@ -4,12 +4,21 @@ use std::io::{self, BufRead};
 fn main() -> io::Result<()> {
     let path = "input/day2.txt";
 
-    println!("{}", solve_part_one(path)?);
+    println!("Part 1: {}", solve_part_one(path)?);
+    println!("Part 2: {}", solve_part_two(path)?);
 
     Ok(())
 }
 
 fn solve_part_one(path: &str) -> io::Result<i32> {
+    io::BufReader::new(File::open(path)?)
+        .lines()
+        .for_each(|f| println!("{}", f.unwrap_or_default()));
+
+    Ok(0)
+}
+
+fn solve_part_two(path: &str) -> io::Result<i32> {
     io::BufReader::new(File::open(path)?)
         .lines()
         .for_each(|f| println!("{}", f.unwrap_or_default()));
