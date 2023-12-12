9a091c604a3bf4ddb0033139bb3c38261f7a0022
Markas Klapovščiuk <klapovsciuk@gmail.com>
2023-12-12 13:12:35 +0200

    feat: (2023) Day 2: Part 1 & 2

--- 2023/src/bin/day2.rs
+++ 2023/src/bin/day2.rs
@@ -11,17 +11,60 @@ fn main() -> io::Result<()> {
 }
 
 fn solve_part_one(path: &str) -> io::Result<i32> {
-    io::BufReader::new(File::open(path)?)
+    let sum = io::BufReader::new(File::open(path)?)
         .lines()
-        .for_each(|f| println!("{}", f.unwrap_or_default()));
+        .enumerate()
+        .filter_map(|(i, line)| line.ok().map(|l| (i as i32 + 1, l)))
+        .filter(|(_, line)| {
+            line.split(": ").nth(1).map_or(false, |reveals| {
+                reveals.split("; ").all(|reveal| {
+                    reveal.split(", ").all(|cube| {
+                        cube.split_once(" ").map_or(false, |(count, color)| {
+                            let count = count.parse::<i32>().unwrap_or(0);
+                            match color {
+                                "red" if count > 12 => false,
+                                "green" if count > 13 => false,
+                                "blue" if count > 14 => false,
+                                _ => true,
+                            }
+                        })
+                    })
+                })
+            })
+        })
+        .map(|(i, _)| i)
+        .sum();
 
-    Ok(0)
+    Ok(sum)
 }
 
 fn solve_part_two(path: &str) -> io::Result<i32> {
-    io::BufReader::new(File::open(path)?)
+    let sum = io::BufReader::new(File::open(path)?)
         .lines()
-        .for_each(|f| println!("{}", f.unwrap_or_default()));
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            line.split(": ").nth(1).map_or(0, |reveals| {
+                let (max_red, max_green, max_blue) = reveals
+                    .split("; ")
+                    .flat_map(|reveal| reveal.split(", "))
+                    .filter_map(|cube| cube.split_once(" "))
+                    .fold(
+                        (0, 0, 0),
+                        |(max_red, max_green, max_blue), (count, color)| {
+                            let count = count.parse::<i32>().unwrap_or(0);
+                            match color {
+                                "red" if count > max_red => (count, max_green, max_blue),
+                                "green" if count > max_green => (max_red, count, max_blue),
+                                "blue" if count > max_blue => (max_red, max_green, count),
+                                _ => (max_red, max_green, max_blue),
+                            }
+                        },
+                    );
 
-    Ok(0)
+                max_red * max_green * max_blue
+            })
+        })
+        .sum();
+
+    Ok(sum)
 }
